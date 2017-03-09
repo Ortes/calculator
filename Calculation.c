38e72ef9baaafe1840529179aa1f033141a3e7e3
Ortes <malo.allee@gmail.com>
2017-03-09 10:25:47 +0000

    Calculation.c correction

--- Calculation.c
+++ Calculation.c
@@ -1,35 +1,23 @@
-
-int calculation(int a, int b, char operation)
+double calculation(int a, int b, char operation)
 {
-    int res = 0;
-
     switch(operation){
-
-        case '+' : res = a + b;
-        break;
-        case '-' : res = a - b;
-        break;
-        case '*' : res = a * b;
-        break;
+        case '+' : return a + b;
+	  break;
+	
+        case '-' : return a - b;
+	  break;
+	
+        case '*' : return a * b;
+	  break;
+	
         case '/' :
-        if ( b == 0)
-        {
-            printf(" division par zero impossible");
-            res = 0;
-        }
-        else
-        {
-            res = a / b;
-        }
+	  //if (b == 0)
+	    //errx(1, "divide by 0");
+	  return a / b;
         break;
-        default : printf(" operateur non accepte");
-        break;
+	
+        default : errx(1, "internal error unknown operator");
+	  break;
     }
     return res;
-}
-
-int main ()
-{
-    calculation(3, 5, '+');
-    return 0;
 }
