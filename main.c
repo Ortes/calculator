3a5ddba3282c20395f7c6fb252ec86d9fe37c554
root <root@localhost.localdomain>
2017-03-11 17:25:05 +0100

    lexer v2.0

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 # include "parsing.h"
 
 int main() {
-  char *s = " 1 + \t4 +sqrt (3 * (1 + 3) #";
+  char *s = " 1 + \t4 +sqrt (3 * (1 + 3) / sin(7 ^6) + n #";
   printf("%d\n", parse(s));
   printf(s);
 }
--- parsing.c
+++ parsing.c
@@ -13,14 +13,17 @@ int chknrmspace(char *s) {
   for (size_t i = 0; i < FUNCTION_NUMBER; i++) is_func[i] = 1;
   int nb_func_left = FUNCTION_NUMBER;
   int pos_in_func_def = 0;
-  printf("%u\n", FUNCTION_NUMBER);
+  printf("%zu\n", FUNCTION_NUMBER);
   
   int state = S_EXP_START;
   int type = T_CALC;
   int parentesis = 0;
   
   for (; *s; ++s) {
-    if (IS_NUMBER(*s)) {
+    if (IS_SPACES(*s))
+      continue;
+
+    else if (IS_NUMBER(*s)) {
       if (state != S_OPERATOR && state != S_EXP_START)
 	return -1;
       state = S_NUMBER;
@@ -32,11 +35,19 @@ int chknrmspace(char *s) {
       else
 	return -1;
     }
+
+    else if (IS_OPERATOR(*s)) {
+      if (state != S_NUMBER && state != S_LETTER)
+	return -1;
+      state = S_OPERATOR;
+    }
     
     else if (IS_LETTER(*s)) {
-      if (state != S_OPERATOR && state != S_EXP_START)
+      if (state != S_OPERATOR && state != S_EXP_START && state != S_LETTER)
 	return -1;
       state = S_LETTER;
+      if (pos_in_func_def == 0 && !IS_LETTER(*(s + 1)))
+	continue;
       for (size_t i = 0; i < FUNCTION_NUMBER; i++) {
 	if (is_func[i] && funcs[i][pos_in_func_def] != *s) {
 	  if (funcs[i][pos_in_func_def]) {
@@ -45,31 +56,34 @@ int chknrmspace(char *s) {
 	  }
 	  else
 	    state = S_FUNC;
+	} else if (is_func[i] && funcs[i][pos_in_func_def + 1] == '\0'){
+	  state = S_FUNC;
+	  for (size_t i = 0; i < FUNCTION_NUMBER; i++) is_func[i] = 1;
+	  pos_in_func_def = -1;
+	  nb_func_left = FUNCTION_NUMBER;
+	  break;
 	}
       }
-      if (nb_func_left <= 0 && pos_in_func_def > 0)
+      if (nb_func_left <= 0)
 	return -1;
       pos_in_func_def++;
     }
 
     else if (*s == '(') {
-      if (state != S_OPERATOR)
+      if (state != S_OPERATOR && state != S_FUNC)
 	return -1;
       parentesis++;
       state = S_EXP_START;
     }
     
     else if (*s == ')') {
-      if (state != S_NUMBER)
+      if (state != S_NUMBER && state != S_LETTER)
 	return -1;
       parentesis--;
       if (parentesis < 0)
 	return -1;
     }
 
-    else if (IS_SPACES(*s))
-      continue;
-    
     else
       return -1;
   }
