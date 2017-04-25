f99afea100db4c7818ed029c0dbf10f56f5cc180
Joshua Dandou <joshua.dandou@epita.fr>
2017-04-25 07:16:00 +0200

    exemple soutenance

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char *argv[]) {
   //char *s = "42.1+23*4/(1+5)";
-  char *e = "x*(x+x*x+1)+x+42";
+  char *e = "42+x*(1+x)";
   //char *a = "1-2";
   //printf("%s\n", s);
   if (argc > 1){
