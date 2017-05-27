afb4069766c47dc19cb589ba630726559bb9ec46
Joshua Dandou <joshua.dandou@epita.fr>
2017-05-27 19:26:04 +0200

    CA FONCTIONNE

--- build_exp.c
+++ build_exp.c
@@ -11,58 +11,59 @@ struct tree* __build_exp(char **s)
   else 
   {
     if (IS_LETTER(**s)) {
-      int nb = sizeof(funcs)/sizeof(funcs[0]);
-      char *s2 = *s;
-      int boolArray[(sizeof(funcs)/sizeof(funcs[0]))];
-      for (int i = 0; i < (sizeof(funcs) / sizeof(funcs[0])); i++) boolArray[i] = 42;
-      int seek = 0;
-      while (nb) {
-        for (int i = 0; i < (sizeof(funcs) / sizeof(funcs[0])); i++) {
-          if (boolArray[i]) {
-            if (funcs[i][seek] != **s) {
-              if (funcs[i][seek] == '\0') {
-                char ch;
-                switch (i) {
-                  case 0: ch = 's';
-                          break;
-                  case 1: ch = 'e';
-                          break;
-                  case 2: ch = 'l';
-                          break;
-                  case 3: ch = '@';
-                          break;
-                  case 4: ch = '#';
-                          break;
-                  case 5: ch = 'j';
-                          break;
-                }
-                (*s)++;
-                op_node = __build_exp(s);
-                struct tree *node = malloc(sizeof(struct tree));
-                node->left = op_node;
-                op_node = node;
-                node = create_node(0);
-                op_node->right = node;
-                store_char(op_node, ch);
-                (*s)--;
-                boolArray[0] == 1337;
-                nb = 0;
-              }
-              else {
-                boolArray[i] = 0;
-                nb--;
-              }
-            }
-          }
-        }
-        (*s)++;
-        seek++;
-      }
-      if (boolArray[0] == 1337)
-      {
+      if (**s == 'n' || **s == 'x') {
         op_node = create_node(0);
         store_char(op_node, **s);
         (*s)++;
+      }
+      else {
+        int nb = sizeof(funcs)/sizeof(funcs[0]);
+        char *s2 = *s;
+        int boolArray[(sizeof(funcs)/sizeof(funcs[0]))];
+        for (int i = 0; i < (sizeof(funcs) / sizeof(funcs[0])); i++) boolArray[i] = 42;
+        int seek = 0;
+        while (nb) {
+          for (int i = 0; i < (sizeof(funcs) / sizeof(funcs[0])); i++) {
+            if (boolArray[i]) {
+              if (funcs[i][seek] != **s) {
+                if (funcs[i][seek] == '\0') {
+                  char ch;
+                  switch (i) {
+                    case 0: ch = 's';
+                            break;
+                    case 1: ch = 'e';
+                            break;
+                    case 2: ch = 'l';
+                            break;
+                    case 3: ch = '@';
+                            break;
+                    case 4: ch = '#';
+                            break;
+                    case 5: ch = 'j';
+                            break;
+                  }
+                  (*s)++;
+                  op_node = __build_exp(s);
+                  struct tree *node = malloc(sizeof(struct tree));
+                  node->left = op_node;
+                  op_node = node;
+                  node = create_node(0);
+                  op_node->right = node;
+                  store_char(op_node, ch);
+                  (*s)--;
+                  boolArray[0] == 1337;
+                  nb = 0;
+                }
+                else {
+                  boolArray[i] = 0;
+                  nb--;
+                }
+              }
+            }
+          }
+          (*s)++;
+          seek++;
+        }
       }
     } 
     else 
@@ -88,10 +89,62 @@ struct tree* __build_exp(char **s)
       op_node->right = __build_exp(s);
     } else {
       if (IS_LETTER(**s)) {
-        op_node->right = create_node(0);
-        store_char(op_node->right, **s);
-        (*s)++;
-      } else {
+        if (**s == 'n' || **s == 'x') {
+          op_node->right = create_node(0);
+          store_char(op_node->right, **s);
+          (*s)++;
+        }
+        else{
+          int nb = sizeof(funcs)/sizeof(funcs[0]);
+          char *s2 = *s;
+          int boolArray[(sizeof(funcs)/sizeof(funcs[0]))];
+          for (int i = 0; i < (sizeof(funcs) / sizeof(funcs[0])); i++) boolArray[i] = 42;
+          int seek = 0;
+          while (nb) {
+            for (int i = 0; i < (sizeof(funcs) / sizeof(funcs[0])); i++) {
+              if (boolArray[i]) {
+                if (funcs[i][seek] != **s) {
+                  if (funcs[i][seek] == '\0') {
+                    char ch;
+                    switch (i) {
+                      case 0: ch = 's';
+                              break;
+                      case 1: ch = 'e';
+                              break;
+                      case 2: ch = 'l';
+                              break;
+                      case 3: ch = '@';
+                              break;
+                      case 4: ch = '#';
+                              break;
+                      case 5: ch = 'j';
+                              break;
+                    }
+                    (*s)++;
+                    struct tree *node = malloc(sizeof(struct tree));
+                    node = __build_exp(s);
+                    struct tree *tmp = malloc(sizeof(struct tree));
+                    tmp->left = node;
+                    tmp->right = create_node(0);
+                    op_node->right = tmp;
+                    store_char(tmp, ch);
+                    (*s)--;
+                    boolArray[0] == 1337;
+                    nb = 0;
+                  }
+                  else {
+                    boolArray[i] = 0;
+                    nb--;
+                  }
+                }
+              }
+            }
+            (*s)++;
+            seek++;
+          }
+        }
+      } 
+      else {
         op_node->right = create_node(atof(*s));
         while (IS_NUMBER(**s) || **s == '.' || **s  == 'e' || **s == 'E') (*s)++;
       }
